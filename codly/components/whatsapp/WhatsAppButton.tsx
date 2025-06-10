f02cd4ccecb4771683d9819bc38faf2ff7cbeeec
codlywebsolutions <codlywebsolutions@gmail.com>
2025-06-10 23:09:24 +0530

    bug fix2

--- codly/components/whatsapp/WhatsAppButton.tsx
+++ codly/components/whatsapp/WhatsAppButton.tsx
@@ -9,9 +9,9 @@ const WhatsAppButton = () => {
       href="https://wa.me/+919947414953" // replace with your WhatsApp number
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-16 right-16 z-50   p-3 rounded-full shadow-lg "
+      className="fixed bottom-10 right-10 z-50   p-3 rounded-full shadow-lg "
     >
-      <Image src={"/w.png"} alt="" height={1000} width={1000} className="md:w-20 md:h-20 h-16 w-16 hover: transition-shadow" /> {/* Replace with WhatsApp icon if needed */}
+      <Image src={"/w.png"} alt="" height={1000} width={1000} className="md:w-20 md:h-20 h-14 w-14 rounded-full" /> {/* Replace with WhatsApp icon if needed */}
     </Link>
   );
 };
